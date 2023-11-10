03d3eb6a443f0b7e2a2b9f4bbd466bf7228cc7e6
Devianochka <105013562+Devianochka@users.noreply.github.com>
2023-11-10 17:00:18 +0300

    Initial commit

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -1,7 +1,23 @@
-from selene.support.shared import browser
-from selene import be, have
+import pytest
+from selene import be, have, browser
 
 
-browser.open('https://google.com')
-browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+@pytest.fixture
+def browser_configs():
+    browser.config.window_width = 1690
+    browser.config.window_height = 1080
+    yield
+    browser.quit()
+
+
+def test_google_find_selene(browser_configs):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+
+def test_google_find(browser_configs):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('tghdsjdkiue').press_enter()
+    browser.element('[class="card-section"]').should(have.text('По запросу tghdsjdkiue ничего не найдено'))
